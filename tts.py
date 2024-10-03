09dffdb69844bac6f90b2ada09958aeeaa21775c
lenovo <2804550190@qq.com>
2024-10-03 21:12:35 +0800

    1

--- tts.py
+++ tts.py
@@ -1,11 +1,22 @@
 import pyttsx3
-import ffmpeg
-def test_to_speech(text: str, output_file: str = "super.wav"):
+from graiax import silkcoder
+
+def text_to_speech(text: str, output_file: str = "super.wav"):
+    # 初始化引擎
     engine = pyttsx3.init()
+
+    # 设置语言
     engine.setProperty('lang', 'zh-CN')
+
+    # 运行
     engine.save_to_file(text, output_file)
     engine.runAndWait()
 
 if __name__ == "__main__":
-    test = '欢迎使用pyttsx3，这是一个将文本转换为语音的库。'
-    test_to_speech(test)
+    # 将文本转换为语音并保存为WAV文件
+    text = "欢迎使用pyttsx3，这是一个将文本转换为语音的库。"
+    text_to_speech(text)
+
+    # 将WAV文件转换为silk格式
+    # convert_wav_to_silk('output.wav', 'output.silk')
+    silkcoder.encode("super.wav", "super.silk")
